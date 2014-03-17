cbfee4c6b731331f2cf690fe0d71005ea8686554
rafael.saraiva@ua.pt <rafael.saraiva@ua.pt@cb70e128-e4b8-4c9d-a067-6e7beed2bc1e>
2014-03-17 22:13:25 +0000

    all good now.
    git-svn-id: https://code.ua.pt/svn/sd-aeroporto-g7@46 cb70e128-e4b8-4c9d-a067-6e7beed2bc1e

--- SDAeroporto/src/Interfaces/PoraoBagageiroInterface.java
+++ SDAeroporto/src/Interfaces/PoraoBagageiroInterface.java
@@ -3,22 +3,23 @@ package Interfaces;
 import Estruturas.Mala;
 
 /**
- * Identifica e descreve as operações que o bagageiro pode realizar sobre o 
+ * Identifica e descreve as operações que o bagageiro pode realizar sobre o
  * monitor <b>Porão</b>
- * 
+ *
  * @author Rafael Figueiredo 59863
  * @author Hugo Frade 59399
  */
 public interface PoraoBagageiroInterface {
+
     /**
      * Tentar recolher uma mala
      * <p>
      * Invocador: bagageiro
      * <p>
-     * O bagageiro desloca-se ao porão do avião e caso este não se encontre vazio
-     * recolhe uma mala
-     * 
-     * @return 
+     * O bagageiro desloca-se ao porão do avião e caso este não se encontre
+     * vazio recolhe uma mala
+     *
+     * @return
      * <ul>
      * <li>Mala que apanhou no porão
      * <li>NULL, caso o porão se encontre vazio
