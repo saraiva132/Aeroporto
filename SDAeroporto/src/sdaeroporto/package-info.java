74a4afae7e411fb51a3ea1058b9f6ed726c6c244
hugofrade@ua.pt <hugofrade@ua.pt@cb70e128-e4b8-4c9d-a067-6e7beed2bc1e>
2014-03-17 16:25:14 +0000

    git-svn-id: https://code.ua.pt/svn/sd-aeroporto-g7@32 cb70e128-e4b8-4c9d-a067-6e7beed2bc1e

---
+++ SDAeroporto/src/sdaeroporto/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the monitors implementation required for the implementation of the 
+ * different interaction zones of a concurrent solution to the problem 
+ * <b>Rapsódia no Aeroporto</b>.
+ */
+
+package sdaeroporto;
