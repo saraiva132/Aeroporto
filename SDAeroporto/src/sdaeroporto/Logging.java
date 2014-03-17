57237a7d4bf584d3643a53f2e02f0d6d77b6bf1d
rafael.saraiva@ua.pt <rafael.saraiva@ua.pt@cb70e128-e4b8-4c9d-a067-6e7beed2bc1e>
2014-03-17 21:58:11 +0000

    mudanças para o logging final
    git-svn-id: https://code.ua.pt/svn/sd-aeroporto-g7@40 cb70e128-e4b8-4c9d-a067-6e7beed2bc1e

--- SDAeroporto/src/sdaeroporto/Logging.java
+++ SDAeroporto/src/sdaeroporto/Logging.java
@@ -3,6 +3,9 @@ package sdaeroporto;
 import Estruturas.AuxInfo.*;
 import static Estruturas.AuxInfo.lotação;
 import static Estruturas.AuxInfo.passMax;
+import Interfaces.LoggingBagageiroInterface;
+import Interfaces.LoggingMotoristaInterface;
+import Interfaces.LoggingPassageiroInterface;
 import java.util.LinkedList;
 
 /**
@@ -12,7 +15,7 @@ import java.util.LinkedList;
  * @author Rafael Figueiredo 59863
  * @author Hugo Frade 59399
  */
-public class Logging {
+public class Logging implements LoggingBagageiroInterface, LoggingMotoristaInterface, LoggingPassageiroInterface {
 
     /**
      * Array com os estados de todos os passageiros
@@ -67,9 +70,10 @@ public class Logging {
     private int[] assentos;
 
     /**
-     * Destino do passageiro.
+     * Destino do passageiro. FDT se for destino final. TRT se estiver
+     * IN_TRANSIT.
      */
-    private destination[] passDest;
+    private String[] passDest;
 
     /**
      * Número de malas total dos passageiros.
@@ -92,15 +96,17 @@ public class Logging {
         fila = new int[passMax];
         nMalasTotal = new int[passMax];
         nMalasActual = new int[passMax];
+        passDest = new String[passMax];
         for (int i = 0; i < passMax; i++) {
             pstate[i] = passState.AT_THE_DISEMBARKING_ZONE;
             nMalasTotal[i] = 0;
             nMalasActual[i] = 0;
             fila[i] = 0;
+            passDest[i] = "";
         }
         assentos = new int[lotação];
         for (int i = 0; i < lotação; i++) {
-            assentos[i] = 0;
+            assentos[i] = -1;
         }
         nMalasStore = 0;
         nMalasBelt = 0;
@@ -114,15 +120,37 @@ public class Logging {
      */
     private void reportInitialStatus() {
         System.out.println("A começar logging...");
-        System.out.println("            Bagageiro                Motorista                  Passageiro1              Passageiro2              Passageiro3              Passageiro4              Passageiro5");
+        System.out.println("PLANE     PORTER                   DRIVER");
+        System.out.println("FN BN  Stat CB SR   Stat  Q1 Q2 Q3 Q4 Q5 Q6  S1 S2 S3");
+        System.out.println("                                                         PASSENGERS");
+        System.out.println("St1 Si1 NR1 NA1 St2 Si2 NR2 NA2 St3 Si3 NR3 NA3 St4 Si4 NR4 NA4 St5 Si5 NR5 NA5 St6 Si6 NR6 NA6");
     }
 
     private synchronized void reportStatus() {
 
-        System.out.printf(nVoo + " %25s %25s ", bstate, mstate);
-        for (int i = 0; i < passMax; i++) {
-            System.out.printf("%25s ", pstate[i]);
+        /*System.out.print(nVoo + " ");
+        System.out.print(nMalasPorao + " ");
+        System.out.print(bstate.toString() + " ");
+        System.out.print(nMalasBelt + " ");
+        System.out.print(nMalasStore + "   ");*/
+        System.out.printf("%2s %2s %10s %2s %2s %10s  Fila de espera: [",nVoo,nMalasPorao,bstate.toString(),nMalasBelt,nMalasStore,mstate.toString());
+        
+        for (int i = 0; i < fila.length; i++) {
+            System.out.printf("%1d,",fila[i]);
         }
+        
+        System.out.print("]  Estado do autocarro: [");
+        for (int i = 0; i < assentos.length; i++) {
+            System.out.printf("%1d,",assentos[i]);
+        }
+        
+        System.out.println("]");
+
+        for (int i = 0; i < passMax; i++) {
+            System.out.printf("%12s %2s %2s %2s",pstate[i].toString(),passDest[i],nMalasTotal[i],nMalasActual[i]);
+        }
+        
+        System.out.println();
         System.out.println();
     }
 
@@ -134,6 +162,7 @@ public class Logging {
      * @param passID identificador do passageiro
      * @param state novo estado do passageiro
      */
+    @Override
     public synchronized void reportState(int passID, passState state) {
         pstate[passID] = state;
         reportStatus();
@@ -146,6 +175,7 @@ public class Logging {
      *
      * @param state novo estado do motorista
      */
+    @Override
     public void reportState(motState state) {
         mstate = state;
         reportStatus();
@@ -158,6 +188,7 @@ public class Logging {
      *
      * @param state novo estado do bagageiro
      */
+    @Override
     public void reportState(bagState state) {
         bstate = state;
         reportStatus();
@@ -172,40 +203,88 @@ public class Logging {
      */
     public synchronized void nVoo(int voo) {
         this.nVoo = voo;
+        reportStatus();
     }
 
-    public void bagagemPorao(int bagagem) {
-        this.nMalasPorao = bagagem;
+    public synchronized void setPorao(int nMalas) {
+        this.nMalasPorao = nMalas;
+        reportStatus();
     }
 
-    public void bagagemBelt(int bagagem) {
-        this.nMalasBelt = bagagem;
+    @Override
+    public void bagagemPorao() {
+        this.nMalasPorao--;
+        reportStatus();
     }
 
-    public void bagagemStore(int bagagem) {
-        this.nMalasStore = bagagem;
+    @Override
+    public synchronized void bagagemBelt(boolean take) {
+        if (take) {
+            this.nMalasBelt--;
+        } else {
+            this.nMalasBelt++;
+        }
+        reportStatus();
     }
 
+    @Override
+    public void bagagemStore() {
+        this.nMalasStore++;
+        reportStatus();
+    }
+
+    @Override
     public synchronized void malasActual(int passID, int nMalas) {
         this.nMalasActual[passID] = nMalas;
+        reportStatus();
     }
 
+    @Override
     public synchronized void malasInicial(int passID, int nMalas) {
         this.nMalasTotal[passID] = nMalas;
+        reportStatus();
     }
 
-    public synchronized void estadoInicial(int passID, destination init) {
-        this.passDest[passID] = init;
-    }
-
-    public synchronized void filaEspera(LinkedList<Integer> fila) {
-        LinkedList<Integer> temp = fila;
-        for (int i = 0; i < fila.size(); i++) {
-            this.fila[i] = temp.getFirst();
+    @Override
+    public synchronized void destino(int passID, Boolean destino) {
+        if (destino) {
+            this.passDest[passID] = "FDT";
+        } else {
+            this.passDest[passID] = "TRT";
         }
+        reportStatus();
     }
-
-    public synchronized void filaEspera(int[] seats) {
+    
+    @Override
+      public synchronized void filaEspera(Object [] oi) {
+        for(int i= 0;i<oi.length;i++)
+        {
+            this.fila[i] = (int)oi[i];
+        }
+        for(int i =1;i<fila.length;i++)
+        {
+            if(fila[i] == fila[i-1])
+                fila[i] = 0;
+        }
+        reportStatus();
+    }
+        public synchronized void filaEsperas(Object [] oi) {   
+        for(int i= 0;i<oi.length;i++)
+        {
+            this.fila[i] = (int)oi[i];
+        }
+        for(int i =1;i<fila.length;i++)
+        {
+            if(fila[i] == fila[i-1])
+                fila[i] = 0;
+        }
+        if(oi.length == 1)
+            this.fila[0] = 0;
+        reportStatus();
+    }
+    @Override
+    public synchronized void autocarroState(int[] seats) {
         System.arraycopy(seats, 0, this.assentos, 0, seats.length);
+        reportStatus();
     }
 }
