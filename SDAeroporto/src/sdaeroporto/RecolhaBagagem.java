44aac78e0c5e4c59ba70444219feb12959e5c6b4
rafael.saraiva@ua.pt <rafael.saraiva@ua.pt@cb70e128-e4b8-4c9d-a067-6e7beed2bc1e>
2014-03-18 15:26:58 +0000

    lala2
    git-svn-id: https://code.ua.pt/svn/sd-aeroporto-g7@55 cb70e128-e4b8-4c9d-a067-6e7beed2bc1e

--- SDAeroporto/src/sdaeroporto/RecolhaBagagem.java
+++ SDAeroporto/src/sdaeroporto/RecolhaBagagem.java
@@ -2,14 +2,15 @@ package sdaeroporto;
 
 import static Estruturas.AuxInfo.*;
 import Estruturas.Mala;
+import Interfaces.LoggingBagageiroInterface;
 import Interfaces.RecolhaBagageiroInterface;
 import Interfaces.RecolhaPassageiroInterface;
 import java.util.HashMap;
 
 /**
- * Monitor que simula a interacção entre os passageiros e o bagageiro na zona
- * de recolha de bagagens e ainda o guichet de reclamação e a zona de 
- * armazenamento de bagagens
+ * Monitor que simula a interacção entre os passageiros e o bagageiro na zona de
+ * recolha de bagagens e ainda o guichet de reclamação e a zona de armazenamento
+ * de bagagens
  *
  * @author Rafael Figueiredo 59863
  * @author Hugo Frade 59399
@@ -17,39 +18,39 @@ import java.util.HashMap;
 public class RecolhaBagagem implements RecolhaBagageiroInterface, RecolhaPassageiroInterface {
 
     /**
-     * Passadeira de bagagens na zona de recolha de bagagens. Cada key do hashmap
-     * está associada a um passageiro e o valor que está associado a essa key 
-     * corresponde ao número de malas que estão na passadeira que pertencem ao 
-     * passageiro
-     * 
+     * Passadeira de bagagens na zona de recolha de bagagens. Cada key do
+     * hashmap está associada a um passageiro e o valor que está associado a
+     * essa key corresponde ao número de malas que estão na passadeira que
+     * pertencem ao passageiro
+     *
      * @serialField belt
      */
     private HashMap<Integer, Integer> belt;
-    
+
     /**
      * Número de malas que estão na zona de armazenamento temporário de bagagens
-     * 
+     *
      * @serialField nMalasStore
      */
     private int nMalasStore;
-    
+
     /**
-     * Identifica se o bagageiro trará mais malas do porão para a zona de recolha 
-     * de bagagens ou não.
-     * 
+     * Identifica se o bagageiro trará mais malas do porão para a zona de
+     * recolha de bagagens ou não.
+     *
      * @serialField noMoreBags
      */
     private boolean noMoreBags;
 
-    
     /**
      * Instanciação e inicialização do monitor <b>RecolhaBagagem</b>
      */
     public RecolhaBagagem() {
         nMalasStore = 0;
         belt = new HashMap<>(nChegadas * passMax);
-        for(int i = 0; i< passMax ; i++)
+        for (int i = 0; i < passMax; i++) {
             belt.put(i, 0);
+        }
         noMoreBags = false;
     }
 
@@ -62,41 +63,33 @@ public class RecolhaBagagem implements RecolhaBagageiroInterface, RecolhaPassage
      * sua mala. Espera até que aviste a sua mala na passadeira rolante ou que o
      * bagageiro anuncie que já não existem mais malas no porão do avião.
      * <p>
-     * Simula, ainda, se o passageiro consegue ou não apanhar a sua mala de forma
-     * bem sucedida.
-     * 
+     * Simula, ainda, se o passageiro consegue ou não apanhar a sua mala de
+     * forma bem sucedida.
+     *
      * @param bagID identificador da mala
-     * @return Forma como conseguiu apanhar a sua mala: 
-     * <ul>
-     * <li>MINE, com sucesso 
-     * <li>UNSUCCESSFUL, sem sucesso 
-     * </ul> 
-     * <p>Alternativamente, a informação de que já não vale a 
-     * pena continuar a espera da(s) sua(s) mala(s) que lhe falta(m):
-     * <ul>
-     * <li>NOMORE
-     * </ul>
+     * @return Forma como conseguiu apanhar a sua mala: <b>com sucesso</b> ou
+     * <b>sem sucesso</b>. Alternativamente, a informação de que já não vale a
+     * pena continuar a espera da(s) sua(s) mala(s) que lhe falta(m)
      */
     @Override
     public synchronized bagCollect goCollectABag(int bagID) {
-        
-            while ( (belt.get(bagID) == 0) && !noMoreBags) { //Dupla condição. Se existir uma mala ou se as malas acabarem
-               try {
+
+        while ((belt.get(bagID) == 0) && !noMoreBags) { //Dupla condição. Se existir uma mala ou se as malas acabarem
+            try {
                 wait();                            //os passageiros são acordados
-                } catch (InterruptedException ex) {
-        }
+            } catch (InterruptedException ex) {
             }
-       if(belt.get(bagID) > 0){
-           belt.put(bagID, belt.get(bagID)-1);
-           if(getBagChance())         
-               return bagCollect.MINE;
-           else 
-               return bagCollect.UNSUCCESSFUL;
-       }
-       return bagCollect.NOMORE;
-        
-            
-        
+        }
+        if (belt.get(bagID) > 0) {
+            belt.put(bagID, belt.get(bagID) - 1);
+            if (getBagChance()) {
+                return bagCollect.MINE;
+            } else {
+                return bagCollect.UNSUCCESSFUL;
+            }
+        }
+        return bagCollect.NOMORE;
+
     }
 
     /**
@@ -106,28 +99,22 @@ public class RecolhaBagagem implements RecolhaBagageiroInterface, RecolhaPassage
      * <p>
      * O bagageiro transporta uma mala para um determinado local:
      * <ul>
-     * <li> para a zona de armazenamento temporário caso a mala pertença a um passageiro 
-     * que esteja em trânsito
+     * <li> para a zona de armazenamento temporário caso a mala pertença a um
+     * passageiro que esteja em trânsito
      * <li> para a passadeira rolante caso pertença a um passageiro cujo destino
      * é este aeroporto, notificando-o de seguida
      * </ul>
      * <p>
-     * Caso o objecto mala seja null notifica todos os passageiros de que já 
-     * não existem mais malas no porão do avião 
-     * 
+     * Caso o objecto mala seja null notifica todos os passageiros de que já não
+     * existem mais malas no porão do avião
+     *
+     * @param log
      * @param mala mala que o bagageiro transporta
-     * @return Local para onde levou a mala:
-     * <ul>
-     * <li>STOREROOM, zona de armazenamento temporário de bagagens
-     * <li>BELT, zona de recolha de bagagens
-     * </ul>
-     * Alternativamente, caso o objecto mala seja um null, a informação de que o porão já se encontra vazio:
-     * <ul>
-     * <li>LOBBYCLEAN
-     * </ul>
+     * @return Local para onde levou a mala, ou caso o objecto mala seja um
+     * null, a informação de que o porão já se encontra vazio
      */
     @Override
-    public synchronized bagDest carryItToAppropriateStore(Mala mala) {
+    public synchronized bagDest carryItToAppropriateStore(LoggingBagageiroInterface log, Mala mala) {
         if (mala == null) {
             //System.out.println("MALAS ACABRAM RAPAZIADA!!!!!");
             noMoreBags = true;
@@ -137,13 +124,19 @@ public class RecolhaBagagem implements RecolhaBagageiroInterface, RecolhaPassage
         //System.out.println("CarryBag "+ mala.getOwner());
         if (mala.inTransit()) {
             nMalasStore++;
+            log.bagagemStore();
             return bagDest.STOREROOM;
         } else {
             //System.out.println(mala.getOwner());
-            belt.put(mala.getOwner(), belt.get(mala.getOwner())+1);
+            belt.put(mala.getOwner(), belt.get(mala.getOwner()) + 1);
+            log.bagagemBelt(false);
             notifyAll();
             return bagDest.BELT;
         }
+    }
+
+    public synchronized void resetNoMoreBags() {
+        this.noMoreBags = false;
     }
 
     /**
@@ -151,11 +144,11 @@ public class RecolhaBagagem implements RecolhaBagageiroInterface, RecolhaPassage
      * <p>
      * Invocador: Passageiro
      * <p>
-     * O passageiro, após não ter coleccionado todas as suas malas e após o 
-     * bagageiro o ter notificado de que já não existem mais malas no porão 
-     * desloca-se ao guichet de reclamação do aeroporto para reclamar a falta da(s)
-     * sua(s) mala(s)
-     * 
+     * O passageiro, após não ter coleccionado todas as suas malas e após o
+     * bagageiro o ter notificado de que já não existem mais malas no porão
+     * desloca-se ao guichet de reclamação do aeroporto para reclamar a falta
+     * da(s) sua(s) mala(s)
+     *
      * @param passageiroID identificador do passageiro
      * @param malasPerdidas número de malas perdidas
      */
@@ -164,15 +157,25 @@ public class RecolhaBagagem implements RecolhaBagageiroInterface, RecolhaPassage
         //System.out.println("Report missing bags. Passageiro: "+passageiroID+" Perdidas: " + malasPerdidas);
         //Imprimir malas perdidas    
     }
-    
+
     /**
      * Função auxiliar que simula a probabilidade de uma mala ser ou não perdida
-     * 
-     * @return TRUE caso a mala não tenha sido perdida
-     *         FALSE caso contrário
+     *
+     * @return TRUE caso a mala não tenha sido perdida FALSE caso contrário
      */
-     private boolean getBagChance() {
+    private boolean getBagChance() {
         return Math.random() > 0.2;
     }
-
+    
+    
+    public synchronized void clearBelt(Logging log)
+    {
+        for (int i = 0; i < passMax; i++) {
+            for(int j = 0;j<belt.get(i);j++)
+            {
+                log.bagagemBelt(true);
+            }
+            belt.put(i, 0);
+        }
+    }
 }
