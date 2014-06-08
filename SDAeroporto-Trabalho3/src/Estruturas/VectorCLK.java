0facc91f2efee4c3fd5c2f17e371cbbc95a521d1
rafael.saraiva@ua.pt <rafael.saraiva@ua.pt@cb70e128-e4b8-4c9d-a067-6e7beed2bc1e>
2014-06-08 22:52:29 +0000

    git-svn-id: https://code.ua.pt/svn/sd-aeroporto-g7@110 cb70e128-e4b8-4c9d-a067-6e7beed2bc1e

--- SDAeroporto-Trabalho3/src/Estruturas/VectorCLK.java
+++ SDAeroporto-Trabalho3/src/Estruturas/VectorCLK.java
@@ -4,16 +4,20 @@ import static Estruturas.Globals.passMax;
 import java.io.Serializable;
 
 /**
- * Identifica o tipo de dados 
- *
+ * Identifica o tipo de dados - Classe que representa um relógio vectorial.
  * @author Rafael Figueiredo 59863
  * @author Hugo Frade 59399
  */
 public class VectorCLK implements Comparable<VectorCLK>, Serializable {
 
-    
+    /**
+     * Mantém os valores dos relógios.
+     */
     private int[] vector;
-
+    
+    /**
+     * Construtor que inicializa os relógios a zero.
+     */
     public VectorCLK() {
         vector = new int[passMax + 2];
         for (int i = 0; i < vector.length; i++) {
@@ -21,19 +25,36 @@ public class VectorCLK implements Comparable<VectorCLK>, Serializable {
         }
     }
     
+    /**
+     * Construtor que recebe um relogio vectorial como argumento.
+     * @param vc - Vector clock que serve como base para a construção da classe.
+     */
     public VectorCLK(VectorCLK vc) {
         vector = new int[passMax + 2];
         this.vector = vc.CloneVector();
     }
-
+    
+    
+    /**
+     * Retorna o relógio vectorial
+     * @return - Relogio vectorial
+     */
     public int[] getVc() {
         return vector;
     }
     
+    /**
+     * Incrementa um relógio.
+     * @param id - Id do relógio a incrementar.
+     */
     public synchronized void Add(int id) {
         vector[id]++;
     }
-
+    
+    /**
+     * Compara dois relógios vectoriais e actualiza o relógio local caso seja necessário.
+     * @param ts - Relógio vectorial para comparar.
+     */
     public synchronized void CompareVector(int[] ts) {
         for (int i = 0; i < vector.length; i++) {
             if (ts[i] > vector[i]) {
@@ -41,7 +62,10 @@ public class VectorCLK implements Comparable<VectorCLK>, Serializable {
             }
         }
     }
-
+    /**
+     * Copia o relógio vectorial e retorna-o.
+     * @return - Cópia do relógio vectorial.
+     */
     public synchronized int[] CloneVector() {
         int[] copia = new int[vector.length];
         for (int i = 0; i < vector.length; i++) {
@@ -49,7 +73,17 @@ public class VectorCLK implements Comparable<VectorCLK>, Serializable {
         }
         return copia;
     }
-
+    /**
+     * Método que efectua override, a partir da interface comparable, do compareTo.
+     * Compara o relógio vectorial local com o recebido por argumento.
+     * @param ts - Relógio com o qual se vai comparar.
+     * @return 
+     * <ul>
+     * <li> -1, se o relógio local estiver desactualizado em relação ao comparado.
+     * <li>  0, caso não se consiga chegar a uma conclusão ou caso os relógios sejam iguais.
+     * <li>  1, se o relógio local estiver mais actualizado que o recebido por argumento.
+     * </ul>
+     */
     @Override
     public int compareTo(VectorCLK ts) {
         boolean older = false, conc = false, newer = false;
